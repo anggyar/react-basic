d348096477324e5cea03ac77c6d4d6b5d78affbc
Anggyar Muhamad Yahya <anggyar.0106@gmail.com>
2025-08-25 16:42:29 +0700

    Add axios for API calls; implement product fetching service; update ProductsPage to display products from API and adjust currency formatting

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,46 +1,54 @@
 import React, { Fragment, useEffect, useState } from "react";
 import CardProducts from "../components/Fragments/CardProducts.jsx";
 import Button from "../components/Elements/Button/index.jsx";
+import { getProductDatas } from "../services/product.service.jsx";
 
-const products = [
-  {
-    id: 1,
-    title: "Nike Air Jordan Blue - Water Hashira Edition",
-    description:
-      "This is a limited edition Nike Air Jordan shoes inspired by the Water Hashira from Demon Slayer. Perfect for collectors and fans alike.",
-    price: 2000000,
-    image: "/images/shoes.jpg",
-  },
-  {
-    id: 2,
-    title: "Copy of Nike Air Jordan",
-    description:
-      "This is a copy of the Nike Air Jordan shoes, designed to look similar but at a more affordable price.",
-    price: 1500000,
-    image: "/images/shoes.jpg",
-  },
-  {
-    id: 3,
-    title: "Nike Air Jordan Blue - Water Hashira Edition",
-    description:
-      "This is a limited edition Nike Air Jordan shoes inspired by the Water Hashira from Demon Slayer. Perfect for collectors and fans alike.",
-    price: 1000000,
-    image: "/images/shoes.jpg",
-  },
-];
+// const products = [
+//   {
+//     id: 1,
+//     title: "Nike Air Jordan Blue - Water Hashira Edition",
+//     description:
+//       "This is a limited edition Nike Air Jordan shoes inspired by the Water Hashira from Demon Slayer. Perfect for collectors and fans alike.",
+//     price: 2000000,
+//     image: "/images/shoes.jpg",
+//   },
+//   {
+//     id: 2,
+//     title: "Copy of Nike Air Jordan",
+//     description:
+//       "This is a copy of the Nike Air Jordan shoes, designed to look similar but at a more affordable price.",
+//     price: 1500000,
+//     image: "/images/shoes.jpg",
+//   },
+//   {
+//     id: 3,
+//     title: "Nike Air Jordan Blue - Water Hashira Edition",
+//     description:
+//       "This is a limited edition Nike Air Jordan shoes inspired by the Water Hashira from Demon Slayer. Perfect for collectors and fans alike.",
+//     price: 1000000,
+//     image: "/images/shoes.jpg",
+//   },
+// ];
 
 const email = localStorage.getItem("email");
 
 export default function ProductsPage() {
   const [cart, setCart] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
     setCart(JSON.parse(localStorage.getItem("cart")) || []);
   }, []);
 
   useEffect(() => {
-    if (cart.length > 0) {
+    getProductDatas((data) => {
+      setProducts(data);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (products.length > 0 && cart.length > 0) {
       const sum = cart.reduce((acc, item) => {
         const product = products.find((product) => product.id === item.id);
         return acc + product.price * item.qty;
@@ -48,7 +56,7 @@ export default function ProductsPage() {
       setTotalPrice(sum);
       localStorage.setItem("cart", JSON.stringify(cart));
     }
-  }, [cart]);
+  }, [cart, products]);
 
   const handleLogout = () => {
     localStorage.removeItem("email");
@@ -86,19 +94,20 @@ export default function ProductsPage() {
       </div>
       <div className='flex justify-center py-5'>
         <div className='w-2/3 flex flex-wrap'>
-          {products.map((product) => (
-            <CardProducts key={product.id}>
-              <CardProducts.Header image={product.image} />
-              <CardProducts.Body title={product.title}>
-                {product.description}
-              </CardProducts.Body>
-              <CardProducts.Footer
-                id={product.id}
-                price={product.price}
-                handleAddToCart={handleAddToCart}
-              />
-            </CardProducts>
-          ))}
+          {products.length > 0 &&
+            products.map((product) => (
+              <CardProducts key={product.id}>
+                <CardProducts.Header image={product.image} />
+                <CardProducts.Body title={product.title}>
+                  {product.description}
+                </CardProducts.Body>
+                <CardProducts.Footer
+                  id={product.id}
+                  price={product.price}
+                  handleAddToCart={handleAddToCart}
+                />
+              </CardProducts>
+            ))}
         </div>
         <div className='w-1/3'>
           <h1 className='text-3xl font-bold text-blue-600 ml-5 mb2'>Cart</h1>
@@ -113,29 +122,30 @@ export default function ProductsPage() {
               </tr>
             </thead>
             <tbody>
-              {cart.map((item) => {
-                const product = products.find((product) => product.id === item.id);
-                return (
-                  <tr key={item.id}>
-                    <td>{product.title}</td>
-                    <td>
-                      Rp.{" "}
-                      {product.price.toLocaleString("id-ID", {
-                        styles: "currency",
-                        currency: "IDR",
-                      })}
-                    </td>
-                    <td>{item.qty}</td>
-                    <td>
-                      Rp.{" "}
-                      {(item.qty * product.price).toLocaleString("id-ID", {
-                        styles: "currency",
-                        currency: "IDR",
-                      })}
-                    </td>
-                  </tr>
-                );
-              })}
+              {products.length > 0 &&
+                cart.map((item) => {
+                  const product = products.find((product) => product.id === item.id);
+                  return (
+                    <tr key={item.id}>
+                      <td>{product.title}</td>
+                      <td>
+                        $
+                        {product.price.toLocaleString("id-ID", {
+                          styles: "currency",
+                          currency: "USD",
+                        })}
+                      </td>
+                      <td>{item.qty}</td>
+                      <td>
+                        $
+                        {(item.qty * product.price).toLocaleString("id-ID", {
+                          styles: "currency",
+                          currency: "USD",
+                        })}
+                      </td>
+                    </tr>
+                  );
+                })}
 
               <tr>
                 <td colSpan={3}>
@@ -143,10 +153,10 @@ export default function ProductsPage() {
                 </td>
                 <td>
                   <b>
-                    Rp.{" "}
+                    $
                     {totalPrice.toLocaleString("id-ID", {
                       styles: "currency",
-                      currency: "IDR",
+                      currency: "USD",
                     })}
                   </b>
                 </td>
---
+++ src/services/product.service.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export const getProductDatas = (callback) => {
+  axios
+    .get("https://fakestoreapi.com/products")
+    .then((response) => callback(response.data))
+    .catch((error) => console.error("Error fetching products:", error));
+};
